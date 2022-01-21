fd798cb9b7b50559589c20fd68aa2d71ce04aa89
José Bezerra <josbezfi@gmail.com>
2022-01-20 21:24:46 -0300

    Add exercices Mario's

--- src/cs50/pset2/exercicios/MarioDesafio.java
+++ src/cs50/pset2/exercicios/MarioDesafio.java
@@ -2,32 +2,42 @@ package cs50.pset2.exercicios;
 
 import java.util.Scanner;
 
-public class Mario {
+public class MarioDesafio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int h;
 
         //Prompt user for height
         do {
-            System.out.println("Height: ");
+            System.out.printf("Height: ");
             h = scanner.nextInt();
         } while (h > 8 || h < 1);
 
-        printLeftAlignedPyramid(h);
+        printPyramid(h);
     }
 
-    private static void printLeftAlignedPyramid(int h) {
+    private static void printPyramid(int h) {
         //Print rows
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < h; j++) {
                 if (i + j < h - 1){
-                    System.out.printf(".");
+                    System.out.printf(" ");
                 } else {
+                    System.out.printf("#");
+                }
+            }
+
+            //Gap between left and right side
+            System.out.printf("  ");
+
+            for (int k = 0; k <= h; k++) {
+                if (i + k >= h){
                     System.out.printf("#");
                 }
             }
             System.out.println();
         }
+
     }
 
 }
